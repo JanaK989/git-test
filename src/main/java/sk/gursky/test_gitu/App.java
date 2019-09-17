8a09cd615bfcc979206b786b6827d2f7bb060f12
student <student@158.197.35.87>
2019-09-17 10:36:06 +0200

    git je super

--- src/main/java/sk/gursky/test_gitu/App.java
+++ src/main/java/sk/gursky/test_gitu/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Europe!" );
+        for (int i = 0; i<5; i++) {
+            System.out.println( "Git je super." );
+
+        }
     }
 }
